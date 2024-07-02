f85229f6df9c4917a266aa300fd00b11ff051a92
中島浩二 <knaka0209@gmail.com>
2024-07-02 21:18:48 +0900

    up, 0702a7

---
+++ sample/turso_todo/README.md
@@ -0,0 +1,31 @@
+﻿# turso todo
+
+ Version: 0.9.2
+
+ Author  : Kouji Nakashima / kuc-arc-f.com
+
+ date    : 2024/03/30 
+
+ update :
+
+***
+
+htmx + vite, Turso LibSQL sample
+
+***
+### Setting
+
+* turso URL, auth token
+
+* .env
+
+```
+TURSO_AUTH_TOKEN=""
+TURSO_URL="libsql://"
+```
+
+***
+### blog
+
+***
+
---
+++ sample/turso_todo/src/App.tsx
@@ -0,0 +1,40 @@
+import { Link, Route, Routes } from 'react-router-dom'
+
+// Auto generates routes from files under ./pages
+// https://vitejs.dev/guide/features.html#glob-import
+const pages = import.meta.glob('./pages/*.tsx', { eager: true })
+
+const routes = Object.keys(pages).map((path) => {
+  const name = path.match(/\.\/pages\/(.*)\.tsx$/)[1]
+  return {
+    name,
+    path: name === 'Home' ? '/' : `/${name.toLowerCase()}`,
+    component: pages[path].default,
+  }
+})
+
+export function App() {
+  return (
+    <>
+      <nav>
+        <ul>
+          {routes.map(({ name, path }) => {
+            return (
+              <li key={path}>
+                <Link to={path}>{name}</Link>
+              </li>
+            )
+          })}
+          <li><a href="/htmx2">Htmx2</a></li>
+          <li><a href="/htmx3">Htmx3</a></li>
+          <li><a href="/htmxtodo">HtmxTodo</a></li>
+        </ul>
+      </nav>
+      <Routes>
+        {routes.map(({ path, component: RouteComp }) => {
+          return <Route key={path} path={path} element={<RouteComp />}></Route>
+        })}
+      </Routes>
+    </>
+  )
+}
